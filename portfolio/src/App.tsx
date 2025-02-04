c71d81bb03f32dec0dbfa59da78f8800ad9425ce
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2025-02-04 08:44:39 +0100

    Manage themes with ThemeProvider

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
 import "./App.css";
-import Cursor from "./components/Cursor/Cursor";
+import Cursor from "./Components/Cursor/Cursor";
+import { ThemeProvider } from "./Contexts/ThemeProvider";
 function App() {
   return (
-    <div className="App">
-      <Cursor />
-      <h1 style={{ color: "white", fontSize: 50 }}>Lucas</h1>
-    </div>
+    <ThemeProvider>
+      <div className="App">
+        <Cursor />
+        <h1 style={{ color: "white", fontSize: 50 }}>Lucas</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+export const toggleTheme = () => {
+  const theme =
+    document.documentElement.getAttribute("data-theme") === "dark"
+      ? "light"
+      : "dark";
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+};
+
+export const initTheme = () => {
+  const savedTheme = localStorage.getItem("theme") || "light";
+  document.documentElement.setAttribute("data-theme", savedTheme);
+};
+
 root.render(
   <React.StrictMode>
     <App />
