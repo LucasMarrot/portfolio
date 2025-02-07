9f2db827fd609e5d8081393edf342a48717d6d15
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2025-02-07 18:06:38 +0100

    Add touch interaction handling for mobile devices in InteractiveObject component

--- src/components/_commons/InteractiveObject/InteractiveObject.tsx
+++ src/components/_commons/InteractiveObject/InteractiveObject.tsx
@@ -25,43 +25,68 @@ export default function InteractiveObject({
   const [isTouched, setIsTouched] = React.useState(false);
 
   React.useEffect(() => {
+    // Manage the different states of the interactive object depending on mobile or desktop
+    // Desktop
     const element = divRef.current;
     if (!element) return;
 
     const handleMouseEnter = () => {
-      setInteractiveState({ type, text });
+      setTimeout(() => {
+        if (isTouched) return;
+        setInteractiveState({ type, text });
+      }, 10);
     };
 
     const handleMouseLeave = () => {
-      setInteractiveState({ type: null });
+      setTimeout(() => {
+        if (isTouched) return;
+        setInteractiveState({ type: null });
+      }, 10);
     };
 
     element.addEventListener("mouseenter", handleMouseEnter);
     element.addEventListener("mouseleave", handleMouseLeave);
 
+    // Mobile
+    const handleTouchStart = (e: TouchEvent) => {
+      if (!isTouched) {
+        element.querySelectorAll("*").forEach((child) => {
+          (child as HTMLElement).style.pointerEvents = "none";
+        });
+        setIsTouched(true);
+      } else {
+        element.querySelectorAll("*").forEach((child) => {
+          (child as HTMLElement).style.pointerEvents = "auto";
+        });
+        setIsTouched(false);
+        setInteractiveState({ type: null });
+      }
+    };
+
+    element.addEventListener("touchstart", handleTouchStart);
+
+    const handleOutsideTouch = (e: TouchEvent) => {
+      if (element && !element.contains(e.target as Node)) {
+        setIsTouched(false);
+      }
+    };
+
+    document.addEventListener("touchstart", handleOutsideTouch);
+
     return () => {
       element.removeEventListener("mouseenter", handleMouseEnter);
       element.removeEventListener("mouseleave", handleMouseLeave);
+      document.removeEventListener("touchstart", handleOutsideTouch);
+      element.removeEventListener("touchstart", handleTouchStart);
       setInteractiveState({ type: null });
     };
-  }, [type, text, setInteractiveState]);
-
-  const handleTouch = (e: React.TouchEvent) => {
-    if (!isTouched) {
-      e.preventDefault();
-      setIsTouched(true);
-    } else {
-      setIsTouched(false);
-      setInteractiveState({ type: null });
-    }
-  };
+  }, [type, text, setInteractiveState, isTouched]);
 
   return (
     <div
       ref={divRef}
       className={`${styles.interactive} ${className}`}
       onClick={() => {}} // TODO : Add the good action here
-      onTouchEnd={handleTouch}
       data-interaction={type}
       data-text={text}
       style={style}
