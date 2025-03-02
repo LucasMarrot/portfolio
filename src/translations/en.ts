03d4ee2f4df0ff0e9019a20f0c5653df99f391fb
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2025-03-02 23:30:13 +0100

    Add navigation translations for English and French languages

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -1,4 +1,9 @@
 export default {
   title: "Hello! This is a test",
   leverText: "Pull the lever!",
+  nav: {
+    home: "Home",
+    projects: "Projects",
+    playground: "Playground",
+  }
 } as const;
--- src/translations/fr.ts
+++ src/translations/fr.ts
@@ -1,4 +1,9 @@
 export default {
   title: "Bonjour ! Ceci est un test",
   leverText: "Tire le levier !",
+  nav: {
+    home: "Accueil",
+    projects: "Projets",
+    playground: "Aire de jeux",
+  }
 } as const;
