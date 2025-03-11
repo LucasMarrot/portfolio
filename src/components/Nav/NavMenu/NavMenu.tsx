7968b8b0530ef4f98b053b5a5db7e2a8a89f867b
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2025-03-11 08:47:47 +0100

    Update NavMenu styles and remove unused NavButton

--- src/components/Nav/NavMenu/NavMenu.tsx
+++ src/components/Nav/NavMenu/NavMenu.tsx
@@ -1,17 +1,14 @@
-import React from "react";
 import NavButton from "../NavButton/NavButton";
 import styles from "./NavMenu.module.scss";
 import { useStrings } from "../../../customHooks/useStrings";
 
 const NavMenu = (): JSX.Element => {
   const strings = useStrings();
-  //TODO: Faire le style
 
   return (
     <ul className={styles.shadowButtonSet}>
       <NavButton label={strings.nav.home} path="/" />
       <NavButton label={strings.nav.projects} path="/projects" />
-      <NavButton label={strings.nav.playground} path="/playground" />
     </ul>
   );
 };
