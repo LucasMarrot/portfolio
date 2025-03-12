3af8008ef2c7846316a090966240d10eee48e710
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2025-03-12 10:51:37 +0100

    Add navigation buttons to ProjectCarousel and update styles

--- src/components/_pages/Projects/Projects.tsx
+++ src/components/_pages/Projects/Projects.tsx
@@ -8,17 +8,17 @@ export default function Projects(): JSX.Element {
   const projects: TProject[] = [
     {
       id: "1",
-      backgroundColor: "#ffadad",
+      backgroundColor: "#1e2824",
       content: <h1 className="text-4xl font-bold">Projet 1</h1>,
     },
     {
       id: "2",
-      backgroundColor: "#ffd6a5",
+      backgroundColor: "#5d3c18",
       content: <h1 className="text-4xl font-bold">Projet 2</h1>,
     },
     {
       id: "3",
-      backgroundColor: "#fdffb6",
+      backgroundColor: "#230000",
       content: <h1 className="text-4xl font-bold">Projet 3</h1>,
     },
   ];
