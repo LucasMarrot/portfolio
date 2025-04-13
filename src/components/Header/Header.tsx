cdde8b578e19907dbf575caaa6d07fe740f9f869
Lucas Marrot <lmarrot@iutbayonne.univ-pau.fr>
2025-04-13 22:47:56 +0200

    Refactor Header and toggle components for improved layout and styling consistency

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,19 @@
-import { useLocation } from "react-router-dom";
 import NavMenu from "../Nav/NavMenu/NavMenu";
-import ThemeToggle from "../Toggles/ThemeToggle/ThemeToggle";
 import LanguageToggle from "../Toggles/LanguageToggle/LanguageToggle";
 import SoundToggle from "../Toggles/SoundToggle/SoundToggle";
 import styles from "./Header.module.scss";
 
 export default function Header(): JSX.Element {
-  const location = useLocation();
-  const isHome: boolean = location.pathname === "/";
-
   return (
     <header className={styles.header}>
       <div className={styles.navMenu}>
         <NavMenu />
       </div>
-      {isHome && (
-        <div className={styles.toggles}>
-          <ThemeToggle />
-          <LanguageToggle />
-          <SoundToggle />
-        </div>
-      )}
+      <div className={styles.toggles}>
+        {/* <ThemeToggle /> */}
+        <SoundToggle />
+        <LanguageToggle />
+      </div>
     </header>
   );
 }
